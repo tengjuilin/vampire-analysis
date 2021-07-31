d8b67308d0cb4197273c1b35321b4d6f58e3a512
tengjuilin <lintengjui@outlook.com>
2021-07-31 13:24:45 +0800

    Update package setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.dev1",
     author="Teng-Jui Lin",
     author_email="lintengjui@outlook.com",
-    description="Visually Aided Morpho-Phenotyping Image Recognition, a robust method to quantify cell morphological heterogeneity",
+    description="Visually Aided Morpho-Phenotyping Image Recognition, a robust method to quantify cell morphological heterogeneity.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tengjuilin/vampire",
