cc09a776af4ea009dfe90d80e792b2dbd0c69a30
Teng-Jui Lin <lintengjui@outlook.com>
2022-03-08 11:01:04 -0800

    Normalize path

--- vampire/tests/test_quickstart.py
+++ vampire/tests/test_quickstart.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import os
 import numpy as np
 import pandas as pd
 import pytest
@@ -37,12 +38,12 @@ def img2_nan_filter():
 
 @pytest.fixture
 def img_set_path():
-    return 'data\\real_img'
+    return os.path.normpath('data\\real_img')
 
 
 @pytest.fixture
 def output_path():
-    return 'data\\quickstart\\output'
+    return os.path.normpath('data\\quickstart\\output')
 
 
 @pytest.fixture
