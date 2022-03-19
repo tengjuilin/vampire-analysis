dbc856d60365df90924c8aaba8376542e8432904
Teng-Jui Lin <lintengjui@outlook.com>
2022-03-18 23:03:57 -0700

    Add coloring and quickstart module

--- vampire/__init__.py
+++ vampire/__init__.py
@@ -1,9 +1,11 @@
 from . import amath
 from . import analysis
+from . import coloring
 from . import extraction
 from . import model
 from . import plot
 from . import processing
+from . import quickstart
 from . import util
 
 __author__ = 'Teng-Jui Lin'
